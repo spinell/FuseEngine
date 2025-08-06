446faec1f5c9cd88bb8fb823c47ece0bd089c2cd
Luc <spiner900@gmail.com>
2025-08-06 03:12:22 -0400

    [SandBox] Create SDL windows

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,6 +1,28 @@
+#include <SDL3/SDL.h>
+
 #include <print>
 
 int main() {
-    std::print("Hello, World!\n");
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS);
+
+    const SDL_WindowFlags windowFlags = SDL_WINDOW_RESIZABLE;
+    SDL_Window*           window = SDL_CreateWindow("FuseEngine Sandbox", 800, 600, windowFlags);
+    if (!window) {
+        std::println("Failed to create SDL window: {}", SDL_GetError());
+        return -1;
+    }
+
+    bool done = false;
+    while (!done) {
+        SDL_Event event{};
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                done = true;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
