60b44530818cf5563935060a971cd9b78f95a980
Luc <spiner900@gmail.com>
2025-08-25 06:19:52 -0400

    [clang-tidy] fix cppcoreguidelines-pro-type-cstyle-cast

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -125,7 +125,7 @@ int main() {
     }
 
     // Load GL extensions using glad
-    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+    if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(SDL_GL_GetProcAddress))) {
         std::println("Failed to initialize the OpenGL context.\n");
         return -1;
     }
@@ -173,8 +173,8 @@ int main() {
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)nullptr);
-    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(float), (void*)20);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 9 * sizeof(float), nullptr);
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 9 * sizeof(float), reinterpret_cast<void*>(20));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
